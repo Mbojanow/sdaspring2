f25ff018f220241d5442b4b6636d10cde32ce5e0
Michal Bojanowski <michal.bojanowski@onegini.com>
2020-01-26 13:43:27 +0100

    asd

--- src/main/java/pl/sdacademy/springdemo/services/PersonService.java
+++ src/main/java/pl/sdacademy/springdemo/services/PersonService.java
@@ -1,4 +1,16 @@
 package pl.sdacademy.springdemo.services;
 
+
+import org.springframework.stereotype.Service;
+
+import pl.sdacademy.springdemo.model.mappers.PersonMapper;
+
+@Service
 public class PersonService {
+
+  private final PersonMapper personMapper;
+
+  public PersonService(final PersonMapper personMapper) {
+    this.personMapper = personMapper;
+  }
 }
