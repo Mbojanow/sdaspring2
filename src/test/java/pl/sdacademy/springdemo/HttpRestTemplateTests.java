2b6d538da4b12da4282e07166b4338348f1c6a66
Michal Bojanowski <michal.bojanowski@onegini.com>
2020-01-26 13:21:12 +0100

    done

--- src/test/java/pl/sdacademy/springdemo/HttpRestTemplateTests.java
+++ src/test/java/pl/sdacademy/springdemo/HttpRestTemplateTests.java
@@ -1,13 +1,25 @@
 package pl.sdacademy.springdemo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import java.util.Base64;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 public class HttpRestTemplateTests {
@@ -46,5 +58,61 @@ public class HttpRestTemplateTests {
     //LinkedHashMap - x form url encoded
     System.out.println(courseResponseEntity.getStatusCode());
     System.out.println(courseResponseEntity.getBody());
+  }
+
+  @Test
+  void whatHappensIfCredentialsAreIncorrect() {
+    restTemplate = new RestTemplateBuilder()
+        .messageConverters(new MappingJackson2HttpMessageConverter())
+        .build();
+
+    final String url = "http://localhost:8080/api/courses/1";
+    assertThatExceptionOfType(HttpStatusCodeException.class)
+        .isThrownBy(() -> restTemplate.getForObject(url, Course.class))
+    .withCause(null);
+  }
+
+  @Test
+  void shouldPassWhenHttpDemoProjectIsNotRunning() {
+    restTemplate = new RestTemplateBuilder()
+        .messageConverters(new MappingJackson2HttpMessageConverter())
+        .build();
+
+    final String url = "http://localhost:8080/api/courses/1";
+    assertThatExceptionOfType(RestClientException.class)
+        .isThrownBy(() -> restTemplate.getForObject(url, Course.class))
+        .withCause(null);
+  }
+
+  @Test
+  void shouldCreateCourse() {
+    final String url = "http://localhost:8080/api/courses";
+    final Course course = new Course(null, "Python", "Pajton kurs", 999L);
+    final ResponseEntity<Void> response = restTemplate.postForEntity(url, course, Void.class);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+  }
+
+  @Test
+  void shouldUpdateCourse() {
+    final String url = "http://localhost:8080/api/courses/{id}";
+    final Course updatedCourse = new Course(1L, "JS", "Node stuff", 499L);
+    restTemplate.put(url, updatedCourse, Map.of("id", 1L));
+  }
+
+  @Test
+  void shouldGetCourseWithCustomHeader() {
+    restTemplate = new RestTemplateBuilder()
+        .messageConverters(new MappingJackson2HttpMessageConverter())
+        .build();
+    final Course course = new Course(null, "Python", "Pajton kurs", 999L);
+
+    final MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+    headers.put("Authorization", List.of("Basic " + Base64.getEncoder().encode(
+        "admin:secret".getBytes()
+    )));
+    HttpEntity<Course> entity = new HttpEntity<>(course, headers);
+
+    restTemplate.postForEntity("http://localhost:8080/api/courses", entity, Void.class);
   }
 }
