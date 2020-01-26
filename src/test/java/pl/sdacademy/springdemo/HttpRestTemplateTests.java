af65f44ad9abb95be69d57daecdd5135e2e7ce70
Michal Bojanowski <michal.bojanowski@onegini.com>
2020-01-26 10:10:34 +0100

    part7

--- src/test/java/pl/sdacademy/springdemo/HttpRestTemplateTests.java
+++ src/test/java/pl/sdacademy/springdemo/HttpRestTemplateTests.java
@@ -1,8 +1,13 @@
 package pl.sdacademy.springdemo;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 public class HttpRestTemplateTests {
@@ -13,6 +18,7 @@ public class HttpRestTemplateTests {
   void setUp() {
     restTemplate = new RestTemplateBuilder()
         .basicAuthentication("admin", "secret")
+        .messageConverters(new MappingJackson2HttpMessageConverter())
         .build();
   }
 
@@ -22,5 +28,23 @@ public class HttpRestTemplateTests {
     final Course course = restTemplate.getForObject(url, Course.class);
 
     System.out.println(course);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  void shouldGetAllCourses() {
+    final String url = "http://localhost:8080/api/courses";
+    final List<Object> courses = restTemplate.getForObject(url, List.class);
+    courses.forEach(System.out::println);
+  }
+
+  @Test
+  void shouldGetEntityOfACourse() {
+    final String url = "http://localhost:8080/api/courses/1";
+    final ResponseEntity<Course> courseResponseEntity = restTemplate.getForEntity(url, Course.class);
+
+    //LinkedHashMap - x form url encoded
+    System.out.println(courseResponseEntity.getStatusCode());
+    System.out.println(courseResponseEntity.getBody());
   }
 }
